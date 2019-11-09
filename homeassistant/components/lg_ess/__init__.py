58ac8011470ae9ad1ecb7a54842c18beb56f0526
paulg <github@pgoergen.de>
2019-11-09 17:56:43 +0100

    add lg_ess component

---
+++ homeassistant/components/lg_ess/__init__.py
@@ -0,0 +1,54 @@
+"""The LG ESS integration."""
+import voluptuous as vol
+
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from .const import DOMAIN
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("password"): str,
+                vol.Required("ip"): str,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass, config):
+    """Legacy setup way."""
+    return True
+
+
+async def async_setup_entry(hass, entry):
+    """Set up the lg_ess component."""
+    from pyess.aio_ess import ESS
+
+    """Set up the LG ESS integration."""
+    # TODO forward the entry for each platform that you want to set up.
+    # hass.async_create_task(
+    #     hass.config_entries.async_forward_entry_setup(entry, "media_player")
+    # )
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    params = entry.data
+    lg_ess = await ESS.create(**params)
+
+    hass.data[DOMAIN][params["ip"]] = lg_ess
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, lg_ess.destruct)
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+
+    #    hass.async_create_task(
+    #        hass.config_entries.async_forward_entry_setup(entry, "switch")
+    #    )
+
+    return True
